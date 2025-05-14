56ccfea2fe6e6afe11cc171631b465775fc9a1dc
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-05-14 15:37:12 +0200

    Added Zustand and AuthState

---
+++ presentation/auth/store/useAuthStore.ts
@@ -0,0 +1,34 @@
+
+import { create } from 'zustand';
+
+import { User } from "@/core/auth/interfaces/user";
+
+export type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking'
+
+export interface AuthState {
+    // Properties
+    status: AuthStatus;
+    token?: string;
+    user?: User;
+
+    // Methods
+    login: (email: string, password: string) => Promise<boolean>
+    checkStatus: () => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>()((set) => ({
+    status: 'checking',
+    token: undefined,
+    user: undefined,
+
+    login: async(email: string, password: string) => {
+        return true;
+    },
+    checkStatus: async() => {
+
+    },
+    logout: async () => {
+
+    }
+}))
