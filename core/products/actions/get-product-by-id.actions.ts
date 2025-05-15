3bbea9818c9294200ba7c0eea3a02c7b585d5a70
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-05-15 10:54:23 +0200

    Added Product actions

---
+++ core/products/actions/get-product-by-id.actions.ts
@@ -0,0 +1,15 @@
+import { API_URL, productsApi } from "@/core/api/productsApi";
+import { type Product } from "../interfaces/product.interface";
+
+export const getProductById = async (id: string): Promise<Product> => {
+    try {
+        const { data } = await productsApi.get<Product>(`/products/${id}`);
+
+        return {
+            ...data,
+            images: data.images.map((image) => `${API_URL}/files/product/${image}`),
+        }
+    } catch (error) {
+        throw new Error('Unable to fetch product with id: ' + id)
+    }
+}
---
+++ core/products/interfaces/product.interface.ts
@@ -0,0 +1,31 @@
+import { User } from "@/core/auth/interface/user";
+
+export interface Product {
+    id:          string;
+    title:       string;
+    price:       number;
+    description: string;
+    slug:        string;
+    stock:       number;
+    sizes:       Size[];
+    gender:      Gender;
+    tags:        string[];
+    user:        User;
+    images:      string[];
+}
+
+export enum Gender {
+    Kid = 'Kid',
+    Men = 'Men',
+    Women = 'Women',
+}
+
+export enum Size {
+    Xs = 'XS',
+    S = 'S',
+    M = 'M',
+    L = 'L',
+    Xl = 'XL',
+    Xxl = 'XXL',
+    Xxxl = 'XXXL'
+}
